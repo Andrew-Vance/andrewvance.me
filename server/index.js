25867efdb58da80f896e46f608812a666c00dce7
Andrew Vance <andrewaddress2@gmail.com>
2021-02-26 20:29:31 -0800

    init

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+//const db = require('../database');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', express.static('client/dist'));
+
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
