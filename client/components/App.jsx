4fdd34c0e491ab0d6bb0eef501ba3fb06197e668
Andrew Vance <andrewaddress2@gmail.com>
2021-03-03 02:20:13 -0500

    added image for future info card

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar.jsx';
 
 const App = (props) => {
   return (
-    <div>a react component</div>
+    <div>
+      <Navbar />
+      <div className='infoCard'>
+        {/* info card */}
+        some div with white background and black text
+        <img className='myImage' src='https://s3.us-east-2.amazonaws.com/andrewvance.me/me_square.png' />
+      </div>
+    </div>
+
   );
 };
 
