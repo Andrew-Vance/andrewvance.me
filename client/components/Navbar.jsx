02e3d0158ec9c5b889a8c83f1d59fb272e966ab5
Andrew Vance <andrewaddress2@gmail.com>
2021-03-03 02:19:48 -0500

    simple navbar, no functionality yet

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = (props) => {
+
+  return (
+    <div className='navbar'>
+      <span>Home</span>
+      <span>Projects</span>
+      <span>Contact Me</span>
+    </div>
+  );
+};
+
+export default Navbar;
