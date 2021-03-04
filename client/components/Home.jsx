1dd67e5feff5c07c5fc044482c98b01a146f163c
Andrew Vance <andrewaddress2@gmail.com>
2021-03-03 19:49:40 -0500

    moved info card to its own view

---
+++ client/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = (props) => {
+
+  return (
+    <div className='infoCard'>
+      {/* info card */}
+      some div with white background and black text
+      <img className='myImage' src='https://s3.us-east-2.amazonaws.com/andrewvance.me/me_square.png' />
+    </div>
+  );
+};
+
+export default Home;
