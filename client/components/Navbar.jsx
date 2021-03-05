55f416ae35715c5bdd8032db21e6e81edf401610
Andrew Vance <andrewaddress2@gmail.com>
2021-03-04 21:25:59 -0500

    now highlighting current view in navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Navbar = ({ updateView }) => {
+const Navbar = ({ updateView, view }) => {
+
+  const currentView = (nav) => {
+    if (nav === view) {
+      return 'selectedNav';
+    }
+  };
 
   return (
     <div className='navbar'>
-      <span onClick={() => updateView('home')}>Home</span>
-      <span onClick={() => updateView('projects')}>Projects</span>
-      <span onClick={() => updateView('contact')}>Contact Me</span>
+      <span className={currentView('home')} onClick={() => updateView('home')}>Home</span>
+      <span className={currentView('projects')} onClick={() => updateView('projects')}>Projects</span>
+      <span className={currentView('contact')} onClick={() => updateView('contact')}>Contact Me</span>
     </div>
   );
 };
