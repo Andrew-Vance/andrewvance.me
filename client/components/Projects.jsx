a8d8db83cb9c2ea4a5826e343eb840e8d986051d
Andrew Vance <andrewaddress2@gmail.com>
2021-03-12 18:05:19 -0500

    added projects view

---
+++ client/components/Projects.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Projects = (props) => {
+
+  return (
+    <div className='projectsPage'>
+      <div className='projects project1'>
+
+      </div>
+      <div className='projects project2'>
+
+      </div>
+      <div className='projects project3'>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Projects;
