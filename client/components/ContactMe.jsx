5c8734ab44b5e2abba73e7af45ebc8694b7b55e7
Andrew Vance <andrewaddress2@gmail.com>
2021-03-14 19:53:52 -0400

    form now sends info to the server and emails it to me

--- client/components/ContactMe.jsx
+++ client/components/ContactMe.jsx
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
+const axios = require('axios');
 
 const ContactMe = (props) => {
 
+  const [name, updateName] = useState('');
+  const [email, updateEmail] = useState('');
+  const [message, updateMessage] = useState('');
+  const [sent, updateSent] = useState(false);
 
   const submit = (e) => {
+    e.preventDefault();
+    updateSent(false);
+    axios.post('/email', {name, email, message})
+      .then(() => {
+        updateSent(true);
+        updateName('');
+        updateEmail('');
+        updateMessage('');
+      })
+      .catch(err => {
+        console.log(err);
+        window.alert('issue sending message, please try again later');
+      })
 
   };
+
+  const displaySent = () => {
+    if (sent) {
+      console.log(sent);
+      return <span className='sentMessage'>SENT</span>;
+    }
+  }
 
   return (
     <div className='contactCard'>
       <div  className='contactForm'>
       <form onSubmit={submit}>
-      <input type='text' size='35' placeholder='NAME' required/><br></br><br></br>
-      <input type='email' pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}" size='35' placeholder='EMAIL' required/><br></br><br></br>
-      <textarea rows='8' cols='30' placeholder='MESSAGE' required></textarea><br></br><br></br><br></br>
+      <input type='text' size='35' placeholder='NAME' value={name} required onChange={(e) => {updateName(e.target.value)}}/><br></br><br></br>
+      <input type='email' pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}" size='35' placeholder='EMAIL' value={email} required onChange={(e) => {updateEmail(e.target.value)}}/><br></br><br></br>
+      <textarea rows='8' cols='30' placeholder='MESSAGE' required value={message} onChange={(e) => {updateMessage(e.target.value)}}></textarea><br></br><br></br><br></br>
       <input type='submit' className='submit' value='Submit' />
       </form>
+      {displaySent()}
       </div>
     </div>
   );
